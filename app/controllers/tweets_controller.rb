ef643f5f3422c5473d2509880b0f0a111a499368
Dinesh Hyaunmikha <dinesh.hyaunmikha@gmail.com>
2022-03-07 19:23:55 +0545

    Delete tweet via turbostream which also remove from UI by matching id (dom_id). Font awesome icons via cdn directly.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,6 +9,16 @@ class TweetsController < ApplicationController
     end
   end
 
+  def destroy
+    @tweet = current_user.tweets.find(params[:id])
+    @tweet.destroy!
+
+    respond_to do |format|
+      format.turbo_stream { render turbo_stream: turbo_stream.remove(@tweet) }
+      format.html { redirect_to root_path }
+    end
+  end
+
   private
 
   def tweet_params
