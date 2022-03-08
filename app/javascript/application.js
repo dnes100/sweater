b9e2823571691870b94f6224a04fe3a60c69dc75
Dinesh Hyaunmikha <dinesh.hyaunmikha@gmail.com>
2022-03-08 19:19:03 +0545

    Added confirm alert before deleting tweet. Override default window.confirm with sweetalert2 with a fix in turbo.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,6 +3,7 @@ import "@hotwired/turbo-rails"
 import "controllers"
 
 import LocalTime from 'local-time'
+import Swal from 'sweetalert2'
 
 // Overwrite with compact style as twitter
 LocalTime.config.i18n.en.time.elapsed = "{time}"
@@ -16,3 +17,24 @@ LocalTime.config.i18n.en.time.singular = "1{time}"
 LocalTime.config.i18n.en.time.singularAn = "1{time}"
 
 LocalTime.start()
+
+
+
+// Note: Override turbo confirm dialog (eg: data-turbo-confirm="Confirm?")
+//   Swal.fire returns a promise which hasn't been handled in turbo.js yet
+//   so we need to add 'await' keyword to 'start' function in turbo.js:
+//   app/assets/javascripts/turbo.js in turbo-rails gem source
+//   const answer = await FormSubmission.confirmMethod(this.confirmationMessage, this.formElement);
+//    Also pin to turbo.js instead of turbo.min.js in config/importmap.rb
+Turbo.setConfirmMethod(
+  async (message, htmlElement) => {
+    const ret = await Swal.fire({
+      title: 'Confirm',
+      text: message,
+      icon: 'question',
+      showCancelButton: true,
+    })
+
+    return ret.isConfirmed
+  }
+)
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,10 +1,11 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
-pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.js", preload: true
 pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.0.1/dist/stimulus.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 
 pin "local-time", to: "https://ga.jspm.io/npm:local-time@2.1.0/app/assets/javascripts/local-time.js"
 pin "tailwindcss-stimulus-components", to: "https://ga.jspm.io/npm:tailwindcss-stimulus-components@3.0.4/dist/tailwindcss-stimulus-components.modern.js"
+pin "sweetalert2", to: "https://ga.jspm.io/npm:sweetalert2@11.4.4/dist/sweetalert2.all.js"
