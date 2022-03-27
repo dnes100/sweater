0d00e0a1f9e1229915c8a69bb364e7016b1e3946
Dinesh Hyaunmikha <dinesh.hyaunmikha@gmail.com>
2022-03-27 22:11:01 +0545

    Hide reply tweets (with non empty parent_id) from home screen.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
   def index
     @tweets = Tweet.where(user: current_user.followings)
-                   .or(Tweet.where(user: current_user))
+      .or(Tweet.where(user: current_user))
+      .where(parent_id: nil)
 
     # Stores ids of liked tweets to reduce db queries
     @liked_tweet_ids = current_user.likes.where(tweet: @tweets).pluck(:tweet_id)
