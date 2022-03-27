9711eaacf4c2e0e43f60bfceb22f89b2dd8a808a
Dinesh Hyaunmikha <dinesh.hyaunmikha@gmail.com>
2022-03-27 23:33:22 +0545

    Simplified layout. Hide reply tweets in user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
 
-    @tweets = @user.tweets
+    @tweets = @user.tweets.where(parent_id: nil)
 
     # Stores ids of liked tweets to reduce db queries
     @liked_tweet_ids = current_user.likes.where(tweet: @tweets).pluck(:tweet_id)
